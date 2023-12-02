1aac1b6a78b972e387714df3a3154a58495ad924
Justin Frank <justinpfrank@protonmail.com>
2023-12-02 17:16:32 -0500

    Make logic update rate independent

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,34 @@ module Main where
 
 import Control.Concurrent
 import Control.Exception
+import Control.Monad
 import System.Console.ANSI
 import System.IO(hFlush, stdout)
 import System.Random
 import System.Random.Stateful
+
+updateDelay :: Int
+updateDelay = 10000
+
+updateDelaySecs :: Float
+updateDelaySecs = fromIntegral updateDelay / 1000000.0
+
+flakeRate :: Float
+flakeRate = 0.05
+
+windDrag :: Float
+windDrag = 10.0 * updateDelaySecs
+
+windIntensity :: Float
+windIntensity = 10.0
+
+windChangeRate :: Float
+windChangeRate = 2*pi*updateDelaySecs / 5.0
+
+flakeDropSpeed :: StatefulGen g m => g -> m Float
+flakeDropSpeed g = do
+  r <- uniformRM (-1, 1) g
+  return $ 6 + r ^ (3::Int)
 
 data FlakeType = Flake1 | Flake2 | Flake3
   deriving (Enum, Bounded)
@@ -38,37 +62,35 @@ data Snowflake = Snowflake FlakeType Int Int Float Float Float Float
 flakePos :: Snowflake -> (Int, Int)
 flakePos (Snowflake _ x y _ _ _ _) = (x, y)
 
-render :: [Snowflake] -> IO ()
-render [] = mempty
-render (Snowflake ft x y _ _ _ _ : rest) = do
+render :: Snowflake -> IO ()
+render (Snowflake ft x y _ _ _ _) = do
   setCursorPosition x y
   putStr $ show ft
-  render rest
+
+clearPos :: (Int, Int) -> IO ()
+clearPos (x, y) = do
+  setCursorPosition x y
+  putChar ' '
 
 runApp :: Int -> Int -> IO ()
 runApp rows cols = do
-  go 0 []
+  go 0.0 []
   where
     go wind flakes = do
-      let (upds, flakes') = update ((sin wind) / 10) flakes
-      mapM_ (\(x, y) -> setCursorPosition x y >> putChar ' ') upds
-      render $ filter (\f -> flakePos f `elem` upds) flakes'
+      let (upds, flakes') = update ((sin wind)^(3 :: Int)) flakes
       gen <- newStdGen
       let new = runStateGen_ gen randomFlakes
-      render new
+      mapM_ clearPos upds
+      mapM_ render $ new ++ flakes'
       hFlush stdout
-      threadDelay 10000
-      dWind <- randomRIO (-pi/1000, pi/1000)
-      go (wind + dWind) $ new ++ flakes'
+      threadDelay updateDelay
+      dWind <- randomRIO (-1.0, 1.0)
+      go (wind + dWind * windChangeRate) $ new ++ flakes'
 
     randomFlakes :: StatefulGen g m => g -> m [Snowflake]
     randomFlakes g = do
-      n <- uniformRM (0 :: Float, 1) g
-      if n > 0.9
-      then do
-        f <- randomFlake g
-        return [f]
-      else return []
+      n <- bernoulli (flakeRate * updateDelaySecs) cols g
+      replicateM n (randomFlake g)
 
     randomFlake :: StatefulGen g m => g -> m Snowflake
     randomFlake g = do
@@ -76,8 +98,8 @@ runApp rows cols = do
       y  <- uniformRM (0, cols-1) g
       ax <- uniformRM (0.0, 0.5) g
       ay <- uniformRM (-1, 1) g
-      dx <- uniformRM (0.03, 0.05) g
-      dy <- uniformRM (-0.02, 0.02) g
+      dx <- flakeDropSpeed g
+      dy <- uniformRM (-2.0, 2.0) g
       return $ Snowflake ft 0 y ax ay dx dy
 
     update :: Float -> [Snowflake] -> ([(Int, Int)], [Snowflake])
@@ -90,8 +112,7 @@ runApp rows cols = do
           where
             f' = moveFlake wind f
             fxy = flakePos f
-            fxy' = flakePos f'
-            x' = fst fxy'
+            fxy'@(x', _) = flakePos f'
 
     moveFlake :: Float -> Snowflake -> Snowflake
     moveFlake wind (Snowflake ft x y ax ay dx dy)
@@ -101,15 +122,21 @@ runApp rows cols = do
         Snowflake ft x' y' ax'' ay'' dx dy'
       | otherwise = Snowflake ft x y ax' ay' dx dy'
       where
-        ax' = ax + dx
-        ay' = ay + dy
+        ax' = ax + dx * updateDelaySecs
+        ay' = ay + dy * updateDelaySecs
         x' = x + truncate ax'
         y' = (y + truncate ay') `mod` cols
         ax'' = ax' - fromIntegral (truncate ax' :: Int)
         ay'' = ay' - fromIntegral (truncate ay' :: Int)
-        dy' = (9 * dy + wind) / 10
+        dy' = (1.0 - windDrag) * dy + windDrag * windIntensity * wind
 
-
+bernoulli :: StatefulGen g m => Float -> Int -> g -> m Int
+bernoulli p n = go n 0
+  where
+    go 0 acc _ = return acc
+    go m acc g = do
+      q <- uniformRM (0, 1) g
+      go (m - 1) (acc + if q < p then 1 else 0) g
 
 setup :: IO ()
 setup = do
