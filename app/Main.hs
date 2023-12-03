03249be5c18c36ac96c87cfa1da5a90719d5d24c
Justin Frank <justinpfrank@protonmail.com>
2023-12-02 20:42:40 -0500

    added snow buildup

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Control.Concurrent
 import Control.Exception
 import Control.Monad
+import Data.Array.IArray
+import Data.List
 import System.Console.ANSI
 import System.IO(hFlush, stdout)
 import System.Random
@@ -42,22 +44,7 @@ instance Show FlakeType where
   show Flake2 = "❅"
   show Flake3 = "❆"
 
-data PileHeight =
-    Pile1 | Pile2 | Pile3 | Pile4
-  | Pile5 | Pile6 | Pile7 | Pile8
-  deriving (Enum, Eq, Ord)
-
-instance Show PileHeight where
-  show Pile1 = "▁"
-  show Pile2 = "▂"
-  show Pile3 = "▃"
-  show Pile4 = "▄"
-  show Pile5 = "▅"
-  show Pile6 = "▆"
-  show Pile7 = "▇"
-  show Pile8 = "█"
-
-data Snowflake = Snowflake FlakeType Int Int Float Float Float Float
+data Snowflake = Snowflake FlakeType !Int !Int !Float !Float !Float !Float
 
 flakePos :: Snowflake -> (Int, Int)
 flakePos (Snowflake _ x y _ _ _ _) = (x, y)
@@ -67,6 +54,23 @@ render (Snowflake ft x y _ _ _ _) = do
   setCursorPosition x y
   putStr $ show ft
 
+renderPile :: Int -> Int -> Int -> IO ()
+renderPile rows col height =
+  go (min (rows * 8) height) (rows-1)
+  where
+    go 0 _ = mempty
+    go 1 h = setCursorPosition h col >> putChar '▁'
+    go 2 h = setCursorPosition h col >> putChar '▂'
+    go 3 h = setCursorPosition h col >> putChar '▃'
+    go 4 h = setCursorPosition h col >> putChar '▄'
+    go 5 h = setCursorPosition h col >> putChar '▅'
+    go 6 h = setCursorPosition h col >> putChar '▆'
+    go 7 h = setCursorPosition h col >> putChar '▇'
+    go n h = do
+      setCursorPosition h col
+      putChar '█'
+      go (n-8) (h-1)
+
 clearPos :: (Int, Int) -> IO ()
 clearPos (x, y) = do
   setCursorPosition x y
@@ -74,19 +78,43 @@ clearPos (x, y) = do
 
 runApp :: Int -> Int -> IO ()
 runApp rows cols = do
-  go 0.0 []
+  go 0.0 (genArray (0, cols-1) (const 0)) []
   where
-    go wind flakes = do
-      let (upds, flakes') = update ((sin wind)^(3 :: Int)) flakes
+    go pWind piles flakes = do
+      let wind = (sin pWind)^(3 :: Int)
+      let (upds, lands, flakes') = update wind piles flakes
+      let newPiles = smooth wind $ accum (+) piles $ map (\i -> (i,1)) lands
       gen <- newStdGen
       let new = runStateGen_ gen randomFlakes
       mapM_ clearPos upds
       mapM_ render $ new ++ filter (\f -> flakePos f `elem` upds) flakes'
+      mapM_ (\i -> renderPile rows i $ newPiles ! i) [0..cols-1]
       setCursorPosition 0 0
       hFlush stdout
-      threadDelay updateDelay
+      threadDelay $ updateDelay
       dWind <- randomRIO (-1.0, 1.0)
-      go (wind + dWind * windChangeRate) $ new ++ flakes'
+      go (pWind + dWind * windChangeRate)
+         newPiles
+         (new ++ flakes')
+
+    smooth :: Float -> Array Int Int -> Array Int Int
+    smooth wind a =
+      accum (+) a $ concat [ updates i | i <- [0..cols-1] ]
+      where
+        updates i
+          | e <= left + 1 && e <= right + 1 = []
+          | left < right = slideLeft
+          | left > right = slideRight
+          | wind >= 0 = slideRight
+          | otherwise = slideLeft
+          where
+            e = a ! i
+            iLeft = if i == 0 then cols-1 else i-1
+            iRight = if i == cols-1 then 0 else i+1
+            left = a ! iLeft
+            right = a ! iRight
+            slideLeft = [(i, -1), (iLeft, 1)]
+            slideRight = [(i, -1), (iRight, 1)]
 
     randomFlakes :: StatefulGen g m => g -> m [Snowflake]
     randomFlakes g = do
@@ -103,17 +131,19 @@ runApp rows cols = do
       dy <- uniformRM (-2.0, 2.0) g
       return $ Snowflake ft 0 y ax ay dx dy
 
-    update :: Float -> [Snowflake] -> ([(Int, Int)], [Snowflake])
-    update wind = foldr updates ([], [])
+    update :: Float -> Array Int Int -> [Snowflake] -> ([(Int, Int)], [Int], [Snowflake])
+    update wind piles = foldl' updates ([], [], [])
       where
-        updates f (upds, fs)
-          | x' < 0 || x' >= rows = (fxy : upds, fs)
-          | fxy == fxy' = (upds, f' : fs)
-          | otherwise = (fxy : fxy' : upds, f' : fs)
+        updates (upds, cs, fs) f
+          | x' < 0 = undefined
+          | x' >= rows - pHeight = (fxy : upds, y' : cs, fs)
+          | fxy == fxy' = (upds, cs, f' : fs)
+          | otherwise = (fxy : fxy' : upds, cs, f' : fs)
           where
             f' = moveFlake wind f
             fxy = flakePos f
-            fxy'@(x', _) = flakePos f'
+            fxy'@(x', y') = flakePos f'
+            pHeight = ((piles ! y') + 7) `div` 8
 
     moveFlake :: Float -> Snowflake -> Snowflake
     moveFlake wind (Snowflake ft x y ax ay dx dy)
